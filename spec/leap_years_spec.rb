06bb82c8b148e1288eadb0c56f6302130993b3ab
Ben Woznicki <b@benWoz.com>
2015-08-26 18:48:48 -0400

    straight & conditional leaps

--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -1,17 +1,34 @@
 class LeapYears
   def leaps?(year)
-  	year % 4 == 0
+  	straight_leap(year) || contidional_leap(year)
+  end
+
+  def straight_leap(year)
+  	(year % 4 == 0) && (year % 100 != 0)
+  end
+
+  def contidional_leap(year)
+  	(year % 4 == 0) && (year % 400 == 0)
   end
 end
 
 RSpec.describe LeapYears, "tell if leap year or not" do
+	year = LeapYears.new
+
+  it "gives true for 4" do 
+    expect(year.leaps?(4)).to eq true    
+ 	end
+
   it "gives true for 400" do 
-  	year = LeapYears.new
     expect(year.leaps?(400)).to eq true    
  	end
 
  	it "gives false for 1" do
- 		year = LeapYears.new
  		expect(year.leaps?(1)).to eq false
  	end
+
+ 	it "gives false for 100" do
+ 		expect(year.leaps?(100)).to eq false
+ 	end
+
 end
